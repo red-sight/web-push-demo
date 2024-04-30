1dd548b3903983ded27ff6c8a87aea66c764afc2
Dmitry Malakhov <dm112@tadbox.com>
2024-04-30 14:04:58 +0300

    lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,30 @@
-// @ts-check
-
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import prettierConfig from "eslint-config-prettier";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const ignores = ["**/dist/", "**/node_modules/**", ".yarn/"];
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   ...tseslint.configs.strict,
-  ...tseslint.configs.stylistic,
+  prettierConfig,
   {
-    ignores: ["**/dist/"],
+    ignores,
     languageOptions: {
       parserOptions: {
         project: true,
-        tsconfigRootDir: import.meta.dirname,
+        tsconfigRootDir: __dirname,
       },
     },
   },
   {
+    ignores,
     files: ["**/*.js", "**/*.mjs", "**/*.cjs"],
     extends: [tseslint.configs.disableTypeChecked],
   }
