0d97d730fcbfcd708ae27fd51e6b219f77bb8397
Dmitry Malakhov <dm112@tadbox.com>
2024-08-05 19:27:56 +0300

    config: env vars for client app

--- apps/client/src/main.ts
+++ apps/client/src/main.ts
@@ -1,12 +1,18 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
 
 import App from './App.vue'
 import router from './router'
+
+console.log(import.meta.env.MODE)
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+  unstyled: true,
+})
 
 app.mount('#app')
